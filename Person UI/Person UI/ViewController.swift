a77d1f18191829f12648e6610781e265a3797aa9
sfikas0221 <stelios.fikas@gmail.com>
2019-02-15 17:48:23 -0600

    Add files via upload
    Implemented the person UI

---
+++ Person UI/Person UI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Person UI
+//
+//  Created by Stelios Fikas on 2/15/19.
+//  Copyright © 2019 Stelios Fikas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
